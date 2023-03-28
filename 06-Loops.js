e8b28e4789ee2735feb6c46fd6f5ff35051e9633
Todd H. Albert <todd@bocacode.com>
2023-03-27 22:35:48 -0400

    more examples for objects and loops

--- 06-Loops.js
+++ 06-Loops.js
@@ -19,3 +19,7 @@ for(let i = 0; i < 10; i++) {
 
 // 1. Create a loop that outputs the numbers 0 through 7.
 
+// 2. Create a loop that counts down from 10 to 1.
+
+// 3. Use a loop to say "I am amazing!" 10 times.
+
